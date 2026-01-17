408018dff83400c0c72eb854fc53b4f544b08df8
kuskyst <126965999+kuskyst@users.noreply.github.com>
2026-01-17 19:34:11 +0900

    Add files via upload

---
+++ rename_by_createdate.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+if "%~1"=="" (
+  echo ファイルをこのbatにドラッグ＆ドロップしてください
+  pause
+  exit /b
+)
+
+for %%F in (%*) do (
+  rem 作成日時を取得（1行まるごと）
+  for /f "tokens=1,2 delims= " %%A in ('dir /TC "%%~fF" ^| find "%%~nxF"') do (
+    set "date=%%A"
+    set "time=%%B"
+  )
+
+  rem 日付整形（YYYYMMDD）
+  set "y=!date:~0,4!"
+  set "m=!date:~5,2!"
+  set "d=!date:~8,2!"
+
+  set "newname=!y!!m!!d!_%%~nxF"
+
+  if not "%%~nxF"=="!newname!" (
+    ren "%%~fF" "!newname!"
+    echo %%~nxF → !newname!
+  )
+)
+
+pause
